2550ffc4c7b86373302fb0364c4c8a23a3ec6a40
Shadow <lasse@dyrsted.dk>
2016-10-15 21:42:00 +0200

    getAccount() test

--- src/Test/PlayerTest.java
+++ src/Test/PlayerTest.java
@@ -2,19 +2,23 @@
 package Test;
 
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 
 public class PlayerTest {
+
+
     Game.Player testPlayer = new Game.Player("Player1");
 
 
-    @org.junit.Test
+    @Test
     public void getName() throws Exception {
         assertEquals("Player1", testPlayer.getName());
     }
 
-    @org.junit.Test
+    @Test
     public void setIsTurn() throws Exception {
         testPlayer.setIsTurn(true);
         assertEquals(true, testPlayer.getIsTurn());
@@ -23,4 +27,8 @@ public class PlayerTest {
         assertEquals(false, testPlayer.getIsTurn());
     }
 
+    @Test
+    public void getAccount(){
+        assertNotNull(testPlayer.getAccount());
+    }
 }
