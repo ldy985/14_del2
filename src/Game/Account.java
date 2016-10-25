0ee902ddbae965035c59929a3ceb075f2388c4de
Shadow <lasse@dyrsted.dk>
2016-10-25 15:43:53 +0200

    Formatting

--- src/Game/Account.java
+++ src/Game/Account.java
@@ -3,15 +3,16 @@ public class Account {
     int balance = 1000;
 
     boolean addBalance(int amount) {
-        balance=balance + amount;
+        balance = balance + amount;
         return true;
     }
 
     int getBalance() {
         return balance;
     }
+
     boolean setBalance(int x) {
-        balance=x;
+        balance = x;
         return true;
     }
 
