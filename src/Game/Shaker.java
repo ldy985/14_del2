47ca9c5f793270c7fc62170442e25d14272daedb
Shadow <lasse@dyrsted.dk>
2016-10-28 13:04:05 +0200

    Revert "Merge branch 'Lasse'"
    This reverts commit f9e350acbfa581c1e8c78838cef6117f14c98571.

--- src/Game/Shaker.java
+++ src/Game/Shaker.java
@@ -1,4 +1,38 @@
+package Game;
 
-    private final Die[] dice;
+public class Shaker {
+
+    //initialising variables
+    private int sum;
+    private final Die[] die;
+
+
+    //The Constructor for the Shaker class
+    public Shaker(int dieCount) {
+        die = new Die[dieCount];
+        for (int i = 0; i < dieCount; i++) {
+            die[i] = new Die();
+        }
+    }
+
+    public void shake() {
+        sum = 0;
+        for (Die die : this.die) {
+            die.roll();
+            sum += die.getFaceValue();
+        }
+    }
+
+
+    public int getSum() {
+        return sum;
+    }
+
     public Die[] getDice() {
-        return dice;
+        return die;
+    }
+
+}
+
+
+
