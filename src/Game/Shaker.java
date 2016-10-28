149cff7f265f0a2676468180179abbdeb94b2315
Shadow <lasse@dyrsted.dk>
2016-10-28 13:06:16 +0200

    Rename

--- src/Game/Shaker.java
+++ src/Game/Shaker.java
@@ -4,20 +4,20 @@ public class Shaker {
 
     //initialising variables
     private int sum;
-    private final Die[] die;
+    private final Die[] dice;
 
 
     //The Constructor for the Shaker class
     public Shaker(int dieCount) {
-        die = new Die[dieCount];
+        dice = new Die[dieCount];
         for (int i = 0; i < dieCount; i++) {
-            die[i] = new Die();
+            dice[i] = new Die();
         }
     }
 
     public void shake() {
         sum = 0;
-        for (Die die : this.die) {
+        for (Die die : this.dice) {
             die.roll();
             sum += die.getFaceValue();
         }
@@ -29,7 +29,7 @@ public class Shaker {
     }
 
     public Die[] getDice() {
-        return die;
+        return dice;
     }
 
 }
