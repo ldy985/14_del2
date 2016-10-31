d5d933ff29861abfbd87f8034b945dc0ccffa13c
Unknown <razzer58@gmail.com>
2016-10-31 10:58:51 +0100

    Commentated the fieldNew class

--- src/Game/FieldNew.java
+++ src/Game/FieldNew.java
@@ -6,14 +6,16 @@ import desktop_fields.Street;
 /**
  * Created by ldylab on 28-10-2016.
  */
+
+// Acts as an extention to the GUI.
+// Makes it possible to set and get the actionText.
 public class FieldNew {
 
     private String title;
-    private String subText;
-    private int rent;
     private String actionText;
+    private int rent;
 
-
+    // Extends the GUI class to be able to set and get actionText.
     public String getActionText() {
         return actionText;
     }
@@ -23,33 +25,24 @@ public class FieldNew {
         return this;
     }
 
+    //Returns rent
     public int getRent() {
         return rent;
     }
 
+    // Sets the rent
     public FieldNew setRent(int rent) {
         this.rent = rent;
         return this;
     }
 
-    public String getTitle() {
-        return title;
-    }
-
+    // Sets the title
     public FieldNew setTitle(String title) {
         this.title = title;
         return this;
     }
 
-    public String getSubText() {
-        return this.subText;
-    }
-
-    public FieldNew setSubText(String subText) {
-        this.subText = subText;
-        return this;
-    }
-
+    // Uses the GUI to set the title, rent, subtext and description.
     public Field toField() {
         Street.Builder a = new Street.Builder()
                 .setTitle(title)
@@ -58,7 +51,7 @@ public class FieldNew {
         if (title.toLowerCase().contains("the") && !title.toLowerCase().contains(" the ")) {
             a.setDescription("This is "+title+". Landing here will result in: ");
         } else {
-            a.setDescription("This is The "+title+". Landing here will result in: ");
+            a.setDescription("This is the "+title+". Landing here will result in: ");
         }
         return a.build();
     }
